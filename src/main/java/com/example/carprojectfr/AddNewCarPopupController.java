77bcd2179212c63c95d89c185b7e900be9ce84c3
Hedzik <igorkosakowski12@gmail.com>
2025-01-14 16:28:12 +0100

    predefined models in popup view.

--- src/main/java/com/example/carprojectfr/AddNewCarPopupController.java
+++ src/main/java/com/example/carprojectfr/AddNewCarPopupController.java
@@ -1,4 +1,25 @@
 package com.example.carprojectfr;
 
+import com.example.carprojectfr.models.Engine;
+import com.example.carprojectfr.models.Gearbox;
+import com.example.carprojectfr.models.PredefinedComponents;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class AddNewCarPopupController {
+    public ComboBox engine_listview;
+    public ComboBox gearbox_listview;
+
+
+    @FXML
+    public void initialize(){
+
+        ObservableList<Gearbox> gearboxes = FXCollections.observableArrayList(PredefinedComponents.getInstance().getGearboxes());
+        ObservableList<Engine> engines = FXCollections.observableArrayList(PredefinedComponents.getInstance().getEngines());
+
+        gearbox_listview.setItems(gearboxes);
+        engine_listview.setItems(engines);
+    }
 }
