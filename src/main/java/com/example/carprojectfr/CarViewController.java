3d205d2aef19b89506b224625e84cc93730a2eca
Hedzik <igorkosakowski12@gmail.com>
2025-01-14 16:03:59 +0100

    Added popup view.

--- src/main/java/com/example/carprojectfr/CarViewController.java
+++ src/main/java/com/example/carprojectfr/CarViewController.java
@@ -1,4 +1,37 @@
 package com.example.carprojectfr;
 
-public class CarViewController {
+import com.example.carprojectfr.models.Car;
+import javafx.scene.control.TextField;
+
+public class CarViewController
+{
+
+
+    public TextField modelField;
+    public TextField registrationField;
+    public TextField weightField;
+    public TextField speedField;
+
+    public TextField gearboxNameField;
+    public TextField gearboxPriceField;
+    public TextField gearboxWeightField;
+    public TextField gearboxGearField;
+
+    public TextField engineNameField;
+    public TextField enginePriceField;
+    public TextField engineWeightField;
+    public TextField engineRpmField;
+
+    public TextField clutchNameField;
+    public TextField clutchPriceField;
+    public TextField clutchWeightField;
+    public TextField clutchConditionField;
+
+    public Car model;
+
+    public void getComponentsDataFromView(){
+
+
+
+    }
 }
